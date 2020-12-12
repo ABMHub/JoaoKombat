90604ff441cf234657d01f2a92362fbb6e5293f0
DYosplay <joaopedrofelix2013@hotmail.com>
2020-12-12 10:51:38 -0300

    io parte 1

---
+++ Mortal Kombat/Componentes/io/KDInterrupt.s
@@ -0,0 +1,271 @@
+########################################################################################################################
+#Procedimento responsável pelo controle do teclado, quando uma tecla é pressionada uma
+#interrupção se inicia. De acordo com a tecla que é pressionada algo pode ou não acontecer.
+#Não há entradas nem saídas aqui
+#
+#Obs_SubZero: Esse procedimento chama dois outros_SubZero: APAGAR e PERSONAGEM
+#
+#			$$$$$$$$ s0, s1, s4, s3 e s5 são alterados$$$$$$$$
+######################################################################################################################## 
+
+KDInterrupt:  
+	addi sp, sp, -4				# aloca espaço na pilha
+	sw ra, 0(sp)				# salva ra na pilha
+	
+	csrrci zero,0,1     			# clear o bit de habilitação de interrupção global em ustatus (reg 0)
+	li t1,0xFF200000    			# Endereço de controle do KDMMIO
+        lw t2,4(t1)             		# le a tecla
+        sw t2,12(t1)            		# escreve no display
+        
+SWITCH_CASE_TECLA: 				
+	li t3, 'd'
+	beq t3, t2, L_DIREITA_IO		# verifica se a tecla pressionada é 'd'
+	
+	li t3, 'a'
+	beq t3, t2, L_ESQUERDA_IO
+	
+	li t3, 'w'
+	beq t3, t2, L_CIMA_IO
+	
+	li t3, 's'
+	beq t3, t2, L_BAIXO_IO
+	
+	li t3, 'x'
+    	beq t3, t2, L_CHUTE_1_IO	        	#verifica se a tecla pressionada é 'x'
+    	
+    	li t3, 'c'
+    	beq t3, t2, L_SOCO_1_IO
+    	
+    	li t3, 'z'
+        beq t3, t2, L_CHUTE_2_IO
+        
+        li t3, 'v'
+        beq t3, t2, L_SOCO_2_IO
+        
+        li t3, 'f'
+        beq t3, t2, L_BLOCK_IO
+        
+        li t3, 'e'
+        beq t3, t2, L_CAMBALHOTA_PRA_FRENTE_IO
+        
+        li t3, 'q'
+        beq t3, t2, L_CAMBALHOTA_PRA_TRAS_IO
+        
+        li t3, ' '
+        beq t3, t2, L_PODER_IO
+        li t1,0xFF200000    			# Endereço de controle do KDMMIO
+	li t0,0x01       			# bit 1 habilita/desabilita a interrupção
+	sw t0,0(t1)           			# Habilita interrupção do teclado
+
+	j Fim_KDInterrupt			# Se não for nenhuma dessas não faz nada
+########################################################################################################################
+L_DIREITA_IO:
+	la t0, AGACHADO_IO			# Carrega o endereço do ponteiro para agachado
+	beq t0, s10, Fim_KDInterrupt		# Se estiver agachado não faz nada
+	
+	la s10, DANCINHA1_IO			# Coloca em s10 o estado atual
+	la t0, CONTADORH1			# carrega o contador
+	lw t1, 0(t0)				# t1 = contador
+	li a3, 0				# inicializa o deslocamento em 0
+	addi t1, t1, 1				# incrementa o contador
+	jal ra, VERIFICA_CONTADOR_DIREITA	# verifica se houve colisão
+	
+	beq a0, zero, L_LIMITE_DIREITA_IO	# a0 != 0 => houve colisão
+	sw t1, 0(t0)				# guarda o novo valor do contador somente se não houve colisão
+	
+	# Se não houve colisão
+	li a3, 4				# deslocamento da caminhada
+	
+L_LIMITE_DIREITA_IO:
+	la t0, CAMINHAR_DIREITA_IO 		# se houve colisão
+	lw a0, 0(t0)				# a0 = sprite da caminhada
+	
+	j CAMINHAR
+########################################################################################################################
+L_ESQUERDA_IO:
+
+	la t0, AGACHADO					
+	beq t0, s10, Fim_KDInterrupt		# se estiver agachado não faz nada
+
+	la s10, DANCINHA1_IO			# Coloca em s10 o estado atual
+	la t0, CONTADORH1			# carrega o contador
+	lw t1, 0(t0)				# t1 = valor do contador
+	li a3, 0				# deslocamento = 0
+	addi t1, t1, -1				# subtrai um do contador
+	jal ra, VERIFICA_CONTADOR_ESQUERDA	# verifica se houve colisão
+	beq a0, zero L_LIMITE_ESQUERDA_IO		# se a0 = 1 => houve colisão
+	sw t1, 0(t0)				# salva o novo contador somente se não houve colisão
+	
+	#Se não houve colisão
+	li a3, -4					# deslocamento da caminhada
+
+L_LIMITE_ESQUERDA_IO:					# Se houve colisão
+	la t0, ESQUERDA_IO				# move pra trás
+	lw a0, 0(t0)					# a0 = sprite a ser pintado
+	j L_CAMINHAR_IO	
+#######################################################################################################################
+L_CIMA_IO:	
+	la t0, DANCINHA1_IO				# Se estiver em pé tem que pular
+	beq t0, s10, L_PULAR_IO
+	
+	la t0, AGACHADO_IO				# se estiver agachado tem que levantar
+	beq t0, s10, L_LEVANTAR_IO
+	
+L_PULAR_IO:
+	# Subindo
+	la t0, PULAR_1_IO				# ponteiro do sprite do pulo
+	lw a0, 0(t0)					# carrega o sprite do pulo
+	li a3, -9600					# desloca 30 pixels para cima
+	li a2, 2					# o pulo são 2 frames
+	jal ra, FRAME_DESLOCAMENTO_VGA			# mostra a animação do pulo
+	
+	# Descendo
+	la t0, PULAR_2_IO				# ponteiro do sprite da descida do pulo
+	lw a0, 0(t0)					# pulo descendo
+	li a3, 9600					# desloca 30 pixels pra baixo
+	
+	li a2, 2					# 2 frames para descida
+	jal ra, FRAME_DESLOCAMENTO_VGA			# mostra a animação dele descendo
+	
+	j DANCINHA_IO					# vai para dancinha
+	
+	# Levantar
+L_LEVANTAR_IO: 
+	la s10, SubZeroParado_1				# faz com que s10 tenha ele em pé
+	la t0, LEVANTAR_IO
+	la a0, 0(t0)					# carrega o sprite dele agaixado
+	li a2, 1					# 1 frame
+	jal ra, FRAME_GOLPE_VGA				# animação
+	
+	j DANCINHA
+#######################################################################################################################
+L_BAIXO_IO:
+	la t0, AGACHADO					# Ponteiro para agachado
+	beq t0, s10, Fim_KDInterrupt			# Se já estiver agachado não faz nada			
+	
+	la t0, DANCINHA1_IO				# Ponteiro para dancinha
+	beq t0, s10, L_AGACHAR_IO			# se estiver em pé tem que abaixar
+
+L_AGACHAR_IO:
+	la t0, AGACHANDO_IO				# carrega o sprite abaixando
+	li a2, 2					# são 2 frames
+	#li a3, 0					# deslocamento 0
+	#jal ra, FRAME_DESLOCAMENTO_VGA			# animação
+	#la s10, AGACHADO_IO				# coloca "agachado" no estado atual
+	
+	#lw a0, 0(s10)					# a0 = sprite dele agachado 
+	#jal ra, FRAME_DANCINHA				# pinta esse sprite na outra frame
+	
+	j GOLPE
+	
+	#j Fim_KDInterrupt_SubZero
+#######################################################################################################################
+L_CHUTE_1_IO:
+    	la t0, DANCINHA1_IO
+   	beq t0, s10, L_CHUTE_1_EM_PE	     	 	# se estiver em pé chuta
+   	
+   	la t0, AGACHADO_IO				# se estiver abaixado dá outro tipo de chute
+	beq t0, s10, L_CHUTE_1_AGACHADO
+	
+L_CHUTE_1_EM_PE:
+	li a2, 5					# são 5 frames
+	
+	# Faz algo obscuro LUCAS
+	li a1, 2					#???????????????????????????????????????
+    	jal ra, TESTE_GOLPE				#???????????????????????????????????????
+    	
+    	la t0, CHUTE_1_EM_PE_IO				# ponteiro do chute 1 normal 
+    	lw a0, 0(t0)					# a0 = chute 1 normal
+    	j GOLPE						# animação
+    	
+L_CHUTE_1_AGACHADO:   
+	li a2, 5					# são 5 frames
+    	la t0, CHUTE_2_AGACHADO_IO			# ponteiro do chute 1 agachado
+    	lw a0, 0(t0)					# a0 = chute 1 agachado	
+    	
+    	j GOLPE
+#######################################################################################################################
+L_SOCO_1_IO:
+    	la t0, DANCINHA1_IO				# ponteiro da dancinha
+    	beq t0, s10, L_SOCO_1_EM_PE		       	# Se estiver em pé dá um soco
+    	
+    	la t0, AGACHADO_IO				# Se estiver abaixado dá outro tipo de soco
+	beq t0, s10, L_SOCO_1_AGACHADO
+
+L_SOCO_1_EM_PE:
+	li a2, 5					# são 2 frames
+   	la t0, SOCO_1_EM_PE_IO				# ponteiro do soco 1 em pé
+   	lw a0, 0(t0)
+   	
+   	#Faz coisas obscuras LUCAS
+   	li a1, 0					#???????????????????????????????????????
+    	jal ra, TESTE_GOLPE				#???????????????????????????????????????
+   	
+   	j GOLPE						# animação
+ 
+L_SOCO_1_AGACHADO:
+	li a2, 3					# são 3 frames
+   	la t0, SOCO_1_AGACHADO_IO			# ponteiro do soco 1 agachado
+   	lw a0, 0(t0)					# sprite do soco 1 agachado
+   	j GOLPE			
+    	
+#######################################################################################################################
+L_CHUTE_2_IO:
+        la t0, DANCINHA1_IO				# Ponteiro da dancinha	
+        beq t0, s10, L_CHUTE_2_EM_PE_IO		        # Se estiver em pé dá um tipo de soco
+        
+        la t0, AGACHADO_IO				# Ponteiro agachado				
+	beq t0, s10, L_CHUTE_2_AGACHADO_IO		# Se estiver abaixado dá outro tipo de soco
+
+L_CHUTE_2_EM_PE_IO:
+	li a2, 6					# são 6 frames
+       	la t0, CHUTE_2_EM_PE_IO				# ponteiro do sprite
+       	lw a0, 0(t0)					# a0 = sprite 
+       	
+       	# Faz coisas obscuras LUCAS		
+       	li a1, 3					#??????????????????????????????
+    	jal ra, TESTE_GOLPE				#??????????????????????????????
+    	
+       	j GOLPE
+
+L_CHUTE_2_AGACHADO_IO:
+	li a2, 5					# são 5 frames
+       	la t0, CHUTE_2_AGACHADO_IO	
+       	lw a0, 0(t0)
+
+	j GOLPE
+#######################################################################################################################
+L_SOCO_2_IO:
+    	la t0, DANCINHA1_IO				# ponteiro da dancinha
+    	beq t0, s10, L_SOCO_2_EM_PE		       	# Se estiver em pé dá um soco
+    	
+    	la t0, AGACHADO_IO				# Se estiver abaixado dá outro tipo de soco
+	beq t0, s10, L_SOCO_2_AGACHADO
+
+L_SOCO_2_EM_PE:
+	li a2, 5					# são 2 frames
+   	la t0, SOCO_2_EM_PE_IO				# ponteiro do soco 1 em pé
+   	lw a0, 0(t0)
+   	
+   	#Faz coisas obscuras LUCAS
+   	li a1, 0					#???????????????????????????????????????
+    	jal ra, TESTE_GOLPE				#???????????????????????????????????????
+   	
+   	j GOLPE						# animação
+ 
+L_SOCO_2_AGACHADO:
+	li a2, 6					# são 6 frames
+   	la t0, SOCO_2_AGACHADO_IO			# ponteiro do soco 1 agachado
+   	lw a0, 0(t0)					# sprite do soco 1 agachado
+   	j GOLPE			
+#######################################################################################################################
+
+L_BLCOK_IO:
+L_CAMBALHOTA_PRA_FRENTE_IO:
+L_CAMBALHOTA_PRA_TRAS_IO:
+L_PODER_IO:
+#######################################################################################################################
+L_CAMINHAR_IO:
+L_DANCINHA_IO:	# pinta a dancinha nas duas frames
+GOLPE:		# SE ESTIVER EM PÉ CHAMA A DANCINHA, SE ESTIVER AGAIXADO PINTA AGACHADO
+Fim_KDInterrupt:
---
+++ Mortal Kombat/Componentes/io/labels.s
@@ -0,0 +1,104 @@
+.data
+	#Labels básicas
+	VGA1INICIO: 			.word 0xFF000000
+	VGA1FINAL: 			.word 0xFF012C00
+	
+	VGA2INICIO:			.word 0xFF100000
+	VGA2FINAL:			.word 0xFF112C00
+
+	PERSONAGEM1: 			.word 0xFF010410
+	PERSONAGEM1_INICIO:		.word 0xFF009158
+	PERSONAGEM1_INICIO_ANTIGO: 	.word 0x0
+	PERSONAGEM1_FINAL: 		.word 0xFF00E4D0	#evite usar
+	
+	PERSONAGEM2: 			.word 0xFF010530	
+	
+	ALTURA_FRAME_0:			.word 0x0
+	ALTURA_FRAME_1:			.word 0x0
+	LARGURA_FRAME_0:		.word 0x0
+	LARGURA_FRAME_1:		.word 0x0
+	
+	ALTURA1:			.word 0x0
+	LARGURA1:			.word 0x0
+	ALTURA1ATUAL:			.word 0x0
+	ALTURA1ANTIGA:			.word 0x0
+	LARGURA1ATUAL:			.word 0x0
+	LARGURA1ANTIGA:			.word 0x0
+	
+	ALTURA_DANCA_0:			.word 0x0
+	LARGURA_DANCA_0:		.word 0x0
+	ALTURA_DANCA_1:			.word 0x0
+	LARGURA_DANCA_1:		.word 0x0
+	
+	SPRITE_DANCA:			.word 0x0
+	
+	CONTADORV1:			.word 0xC
+	CONTADORH1:			.word 0x1
+	CONTADORV2:			.word 0xC
+	CONTADORH2:			.word 0x18
+			
+	MATRIZ_COMBATE:			.space 300
+	
+	#########################################
+	FILA_PERSONAGEM_1:		.space 120
+	
+	# Labels 
+	AGACHADO_IO:			.word 0x0	# ELE LITERALMENTE AGACHADO, NÃO É O PRIMEIRO SPRITE DO AGACHAMENTO
+	AGACHANDO_IO:			.word 0x0	# PRIMEIRO SPRITE DELE AGACHANDO
+	PULAR_1_IO:			.word 0x0	# Subindo
+	PULAR_2_IO:			.word 0x0	# Descendo
+	LEVANTAR_IO:			.word 0x0 
+	CAMINHAR_DIREITA_IO:		.word 0x0
+	CAMINHAR_ESQUERDA_IO:		.word 0x0
+	CAMBALHOTA_DIREITA_IO:		.word 0x0
+	CAMBALHOTA_ESQUERDA_IO:		.word 0x0
+	
+	CHUTE_1_EM_PE_IO:		.word 0x0
+	CHUTE_1_AGACHADO_IO:		.word 0x0
+	SOCO_1_EM_PE_IO:			.word 0x0
+	SOCO_1_AGACHADO_IO:		.word 0x0
+
+	SOCO_2_EM_PE_IO:			.word 0x0
+	SOCO_2_AGACHADO_IO:		.word 0x0
+
+	CHUTE_2_EM_PE_IO:			.word 0x0
+	CHUTE_2_AGACHADO_IO:			.word 0x0
+	ALPISTE_ORH_IO:			.word 0x0
+	#PODER_IO			.word 0x0
+	JAB_IO:				.word 0x0
+	PARADO_IO:			.word 0x0
+	DANCINHA1_IO:			.word 0x0
+	DANCINHA2_IO:			.word 0x0
+	BLOCK_EM_PE_IO:			.word 0x0
+	BLOCK_AGACHADO_IO:		.word 0x0
+	DESATIVAR_BLOCK_EM_PE_IO:	.word 0x0
+	DESATIVAR_BLOCK_AGACHADO_IO:	.word 0x0
+	CAMBALHOTA_FRENTE_IO:		.word 0x0
+	CAMBALHOTA_TRAS_IO:		.word 0x0
+	
+	AGACHADO_IA:			.word 0x0
+	PULAR_IA:			.word 0x0
+	LEVANTAR_IA:			.word 0x0 
+	CAMINHAR_DIREITA_IA:		.word 0x0
+	CAMINHAR_ESQUERDA_IA:		.word 0x0
+	CAMBALHOTA_DIREITA_IA:		.word 0x0
+	CAMBALHOTA_ESQUERDA_IA:		.word 0x0
+	
+	CHUTE_NORMAL_IA:		.word 0x0
+	CHUTE_AGACHADO_IA:		.word 0x0
+	SOCO_NORMAL_IA:			.word 0x0
+	SOCO_AGACHADO_IA:		.word 0x0
+	CHUTE_ALTO_IA:			.word 0x0
+	RASTEIRA_IA:			.word 0x0
+	ALPISTE_ORH_IA:			.word 0x0
+	#PODER_IA			.word 0x0
+	JAB_IA:				.word 0x0
+	PARADO_IA:			.word 0x0
+	DANCINHA1_IA:			.word 0x0
+	DANCINHA2_IA:			.word 0x0
+	BLOCK_EM_PE_IA:			.word 0x0
+	BLOCK_AGACHADO_IA:		.word 0x0
+	DESATIVAR_BLOCK_EM_PE_IA:	.word 0x0
+	DESATIVAR_BLOCK_AGACHADO_IA:	.word 0x0
+	CAMBALHOTA_FRENTE_IA:		.word 0x0
+	CAMBALHOTA_TRAS_IA:		.word 0x0
